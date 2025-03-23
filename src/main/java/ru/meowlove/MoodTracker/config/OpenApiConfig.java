2453746abb4edd4ba2881ba65c46775c66c89596
Morkofff <124193334+Morkofff@users.noreply.github.com>
2025-03-23 20:53:57 +0400

    update: version

--- src/main/java/ru/meowlove/MoodTracker/config/OpenApiConfig.java
+++ src/main/java/ru/meowlove/MoodTracker/config/OpenApiConfig.java
@@ -7,7 +7,7 @@ import io.swagger.v3.oas.annotations.info.Info;
         info = @Info(
                 title = "Mood Tracker",
                 description = "API for Mood Tracker (Seishu's individual project)",
-                version = "Alpha 0.3.23"
+                version = "1.0.4"
         )
 )
 public class OpenApiConfig {
